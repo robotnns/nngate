c58d1a23e6977379944178db1af14b4437c4ca4a
dev1 <nomail@nomail.com>
2017-01-03 15:47:18 +0100

    feature viewer ok scal color

--- libsfviewer/pixel_matrix_viewer.h
+++ libsfviewer/pixel_matrix_viewer.h
@@ -7,30 +7,46 @@
 class image_vec 
 {
 	public:
-	image_vec(const int &height,const int &widths ,double &threshold,float &posx,float &posy, std::vector<double> *pix_vec)
+	image_vec(const int &height,const int &widths ,float &posx,float &posy, std::vector<double> *pix_vec,float scale)
 	{
 		_height = height;
 		_widths = widths;
-		_threshold=threshold;
+		
 		_pix_vec=pix_vec;
 		_posx=posx;
 		_posy=posy;
+		_scale=scale;
+		//find min max
+		_vec_min=pix_vec->at(0);
+		_vec_max=pix_vec->at(0);		
+		for (auto &e : (*pix_vec))
+		{
+			if(e>_vec_max)
+			_vec_max=e;
+			if(e<_vec_min)
+				_vec_min=e;
+			
+		}	
+		std::cout <<"min:" << _vec_min <<"max:"<<_vec_max<<std::endl;
 	}
 	int _height;
 	int _widths;
 	float _posx=0;
 	float _posy=0;
+	float _scale=1.0;
+	double _vec_min;
+	double _vec_max;
 	
-	double _threshold;
+	
 	std::vector<double> *_pix_vec;
 };
 class PixelMatrixViewer
 {
 	    
 		public :
-		void add_vec(const int &&height,const int &&widths ,double &&threshold,float &&posx,float &&posy,std::vector<double> *pix_vec)
+		void add_vec(const int &&height,const int &&widths ,float &&posx,float &&posy,std::vector<double> *pix_vec,float scale=1.0)
 		{
-			auto iv =  image_vec(height,widths,threshold,posx,posy,pix_vec);
+			auto iv =  image_vec(height,widths,posx,posy,pix_vec,scale);
 			v_im.push_back(iv);
 		}
 		void clear_vec()
@@ -41,12 +57,15 @@ class PixelMatrixViewer
 		
 		
 		std::vector<image_vec> v_im;
-	void init(int wh,int wl)
+	void init(int wh,int wl,bool rgb_color=false)
 	{
 		 window.create(sf::VideoMode(wh, wl), "viewer");
 		 window.setFramerateLimit(60);
+		 _rgb_color=rgb_color;
 		
 	}
+
+	
 void draw_image_lib(int index)
 {
 
@@ -55,41 +74,54 @@ void draw_image_lib(int index)
     sf::Sprite       sprite;
     sf::Texture texture;
     sf::Uint8        *pixels  = new sf::Uint8[v_im.at(index)._widths*v_im.at(index)._height* 4];
-
-	std::cout << "size:"<<v_im.at(index)._pix_vec->size() << std::endl;
-       for (int y=0 ; y<v_im.at(index)._widths-1  ; y++)
+    auto &img  = v_im.at(index);
+	//std::cout << "size:"<<v_im.at(index)._pix_vec->size() << std::endl;
+       for (int y=0 ; y<img._widths-1  ; y++)
 		{
 		for (int x=0 ; x<v_im.at(index)._height ; x++)
 		 {
-			std::cout << "x:"<< x << "y:" << y << std::endl;
-			   double color  =v_im.at(index)._pix_vec->at(v_im.at(index)._widths*y+x);
+		//	std::cout << "x:"<< x << "y:" << y << std::endl;
+			   double color  =img._pix_vec->at(img._widths*y+x);
 		   /*if(color>v_im.at(index)._threshold)
 			   image.setPixel (x,  y, sf::Color(255, 255, 255));
                 else
                 image.setPixel ( x,  y, sf::Color(0, 0, 255));*/
 					//image.setPixel (x,y,grayToRgb((unsigned char)color));
-				//std::cout << "COLOR = > " << 	color << std::endl;				
-				image.setPixel (x,y,grayToRgb(color));
+				//std::cout << "COLOR = > " << 	color << std::endl;	
+				if(_rgb_color)
+				image.setPixel (x,y,grayToRgb(color,img._vec_min,img._vec_max));
+				else
+				image.setPixel (x,y,oneColorGreen(color,img._vec_min,img._vec_max));
             }
 			
 			
         }
 
-texture.loadFromImage(image);
-sprite.setTexture(texture, true);
-sprite.setPosition(v_im.at(index)._posx,v_im.at(index)._posy);
-        window.draw(sprite);
+	texture.loadFromImage(image);
+	sprite.setTexture(texture, true);
+	sprite.setPosition(img._posx,img._posy);
+	if(img._scale!=1.0)
+	sprite.setScale(img._scale,img._scale);
+    window.draw(sprite);
 
 
 }
 
-sf::Color grayToRgb(double val)
+sf::Color grayToRgb(double val,double min , double max)
 {
-	unsigned char ramapedVal = remapValue(val ,-1.0,1.0,0,255);
+	unsigned char ramapedVal = remapValue(val ,min,max,0,255);
 	return grayToRgb(ramapedVal);
 	
 }
 
+
+sf::Color oneColorGreen(double val,double min , double max)
+{
+	unsigned char ramapedVal = remapValue(val ,min,max,0,255);
+	return sf::Color(0,ramapedVal,0);
+	
+	
+}
 sf::Color grayToRgb(unsigned char Color )
 {
 	int Red   = (Color >> 5) * 255 / 7;
@@ -116,6 +148,7 @@ void render()
 }
 private:
 sf::RenderWindow window;
+bool _rgb_color=false;
 };
 
 
--- libsfviewer/test.cpp
+++ libsfviewer/test.cpp
@@ -1,15 +1,23 @@
 #include "pixel_matrix_viewer.h"
 #include <unistd.h>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+double frand_a_b(double a, double b){
 
+    return (( rand()/(double)RAND_MAX ) * (b-a) + a);
+}
 int main()
 {
 	
-	
+	 srand (time(NULL));
 	std::vector<double> pix_vec;
-	for(int i = 0 ; i < 400;i++)
-	pix_vec.push_back(0.224	);
+	for(int i = 0 ; i < 800;i++)
+	pix_vec.push_back(frand_a_b(-1.0,1.0)	);
+
+
 	PixelMatrixViewer pxv;
-	std::cout << "before11"<<std::endl;
+	
 	
 	/*float fcolor =pxv.remapValue(0.22 ,-1.0,1.0,0,255);
 	unsigned char color = fcolor;
@@ -17,10 +25,8 @@ int main()
 	exit(0);*/
 	
 	pxv.init(800,600);
-	pxv.add_vec(20,20,0.0,10.0,10.0,&pix_vec);
-	pxv.add_vec(20,20,0.0,50.0,50.0,&pix_vec);
-	pxv.add_vec(20,20,0.0,100.0,100.0,&pix_vec);
-	pxv.add_vec(20,20,0.0,200.0,200.0,&pix_vec);
+	pxv.add_vec(20,20,10.0,10.0,&pix_vec,2.0);
+	pxv.add_vec(20,20,50.0,50.0,&pix_vec);
 	pxv.render();	
 	sleep(4);
 	
